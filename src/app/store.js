8f3b7d111081243e7797181d6f495e2b62b388cd
Mohmaed Amine Chalouati <mohamedaminechalouati@gmail.com>
2023-02-05 13:21:18 +0100

    updating the store

--- src/app/store.js
+++ src/app/store.js
@@ -1,6 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
+import searchTermReducer from '../features/searchBar/searchBarSlice';
 
 export const store = configureStore({
   reducer: {
+    searchTerm: searchTermReducer,
   },
 });
